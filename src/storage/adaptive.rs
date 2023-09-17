5f744a83a44bf2d4b52f38f99a28dfcbb4e525dd
dvdsk <noreply@davidsk.dev>
2023-09-17 23:24:29 +0200

    style(storage): autoformatted

--- src/storage/adaptive.rs
+++ src/storage/adaptive.rs
@@ -28,10 +28,7 @@ where
     /// Creates a new [`AdaptiveStorageProvider`]. The supplied size is used to construct a
     /// [`BoundedStorageReader`] when the stream doesn't have a known content length.
     pub fn new(inner: T, size: NonZeroUsize) -> Self {
-        Self {
-            inner,
-            size,
-        }
+        Self { inner, size }
     }
 }
 
