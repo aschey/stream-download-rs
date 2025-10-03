1812ab2665f2de8311d522cc7d146b9dcf93667e
Austin Schey <aschey13@gmail.com>
2025-10-02 22:30:43 -0700

    chore: remove doc_auto_cfg

--- crates/stream-download-opendal/src/lib.rs
+++ crates/stream-download-opendal/src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(missing_docs)]
 #![forbid(clippy::unwrap_used)]
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 #![doc = include_str!("../README.md")]
 
 use std::fmt::Debug;
